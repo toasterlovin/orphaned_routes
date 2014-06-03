6a4e9022c97843a1d8ab0e3b2fe5946112900175
Rico Jones <rico@toasterlovin.com>
2014-06-03 15:08:47 -0700

    Add routes to show bonus behaviors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :things
+
+  # No controller exists
+  # resources :doodads
+
+  # Action magically created because view exists
   # resources :widgets
 end
